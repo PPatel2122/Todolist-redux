c0057c9bac83154b8f43cb72fdf76bb92be15f8c
PAWANPATEL <pp467354@gmail.com>
2024-11-29 15:03:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+import Addtodo from './Addtodo'
+import TodoList from './TodoList'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Addtodo/>
+    <TodoList/>
+
+    </>
+  )
+}
+
+export default App
